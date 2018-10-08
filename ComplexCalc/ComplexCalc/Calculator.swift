2ca2e677e3b63f98db84083a1b1f69fc332c07bb
Kyle Wistrand <kyle.wistrand@gmail.com>
2018-10-07 23:26:05 -0700

    Completing basic tests

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,36 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    public func add(_ args: [Double]) -> Double {
+        // Inspiration: https://stackoverflow.com/a/24795181
+        return args.reduce(0, +)
+    }
+    
+    public func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    public func multiply(_ args: [Double]) -> Double {
+        return args.reduce(1, *)
+    }
+    
+    public func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+    
+    public func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    public func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    public func count(_ args: [Double]) -> Int {
+        return args.count
+    }
+    
+    public func avg(_ args: [Double]) -> Double {
+        return Double(add(args)) / Double(count(args))
+    }
 }
